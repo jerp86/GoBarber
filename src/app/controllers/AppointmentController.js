eeafc6993ff0c16a96fa7988d180e6ceda435103
jerp86 <jerp4@hotmail.com>
2020-02-18 06:58:17 -0300

    Listando notificações do usuário

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -49,6 +49,15 @@ class AppointmentController {
     const { provider_id, date } = req.body;
 
     /**
+     * Check user_id is equal a provider_id
+     */
+    if (provider_id === req.userId) {
+      return res.status(401).json({
+        error: 'You can only create appointments with others providers',
+      });
+    }
+
+    /**
      * Check if procider_id is a provider
      */
     const isProvider = await User.findOne({
